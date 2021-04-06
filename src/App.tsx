a62ca049686480fd75205b1182482dc4298583cd
FledgeShiu <xzk0701@gmail.com>
2021-04-06 13:29:44 +0800

    Init.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.sass";
+import { BiFolderOpen, BiHistory, BiCalendar } from "react-icons/bi";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import Subscription from "./Subscription";
+import History from "./History";
+import Today from "./Today";
 function App() {
+  const sidebarIconSize = 42;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <BrowserRouter>
+      <div className="App">
+        <nav
+          className="navbar card bd-navbar is-fixed-top"
+          role="navigation"
+          aria-label="main navigation"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="navbar-brand">
+            <div className="navbar-brand ">
+              <div className="navbar-item nav-logo">Mashiro</div>
+            </div>
+          </div>
+          <div className="navbar-menu">
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <div className="field has-addons">
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="text"
+                      placeholder="Find a repository"
+                    />
+                  </div>
+                  <div className="control">
+                    <button className="button is-info">Search</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <div className="sidebar card">
+          <ul className="sidebar-menu">
+            <li>
+              <Link to="/subscription" className="sidebar-link">
+                <BiFolderOpen size={sidebarIconSize} />
+              </Link>
+            </li>
+            <li>
+              <Link to="/history" className="sidebar-link">
+                <BiHistory size={sidebarIconSize} />
+              </Link>
+            </li>
+            <li>
+              <Link to="/today" className="sidebar-link">
+                <BiCalendar size={sidebarIconSize} />
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <Switch>
+            <Route path="/subscription">
+              <Subscription />
+            </Route>
+            <Route path="/history">
+              <History />
+            </Route>
+            <Route path="/today">
+              <Today />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Subscription.tsx
@@ -0,0 +1,3 @@
+export default function Subscription() {
+  return <div>Subscription</div>;
+}
